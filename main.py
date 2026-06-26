5ec727e8e8f2b77c74e6b1bac00ca6f706cd706d
Guilherme <guiziy3424@gmail.com>
2026-06-26 00:01:23 -0300

    Create requirements.txt

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+
+print('Setup start')
+pygame.init()
+window = pygame.display.set_mode((600, 480))
+print('Setup end')
+
+print('Loop start')
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print('Quitting')
+            pygame.quit()
+            break
+
